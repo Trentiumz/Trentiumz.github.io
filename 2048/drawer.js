2261b14e54011be25f070565db45d44cdb3fffdb
Daniel Ye <danielyeqt@gmail.com>
2021-04-05 20:16:46 -0400

    Add Comments

--- 2048/drawer.js
+++ 2048/drawer.js
@@ -1,23 +1,32 @@
 // GLOBAL VARIABLES --------------------------------------------
 
+// Canvas & Drawing; length = canvas width
 var canvas
 var ctx
 var length
 
+// Line Width & Color; the margins of each tile from the lines
 const lineWidthP = 4;
 const lineColor = "#8a6d5c";
 const cellMarginFromLineCenter = 1;
 
+// The prefix used to represent each image
 const numberImagePrefix = "img"
 
+// Starting Size
 const startSizeAnimationRatio = 0.8
+// frames in moving, frames for the creation animation
 const animationMovingFrames = 5
 const animationCreationFrames = 5
+// Milliseconds between animations
 const animationDelta = 15
 
+// The width of each tile
 const numberWidth = (100 - lineWidthP) / numSquares - 2 * cellMarginFromLineCenter;
+// The interval at which we place the tiles
 const cellInterval = (100 - lineWidthP) / numSquares
 
+// Preferred Name of Image: ID of image in our html
 var imagePaths = {
   border: "border",
   textBox: "finishedBox"
@@ -25,6 +34,7 @@ var imagePaths = {
 // Elements should be put in as a string, just to stay consistent
 var images = {};
 
+// Whether or not we're currently in the middle of an animation
 var handlingAnimation = false
 
 
@@ -33,21 +43,28 @@ var handlingAnimation = false
 // initialize variables and stuff
 function initialize() {
   console.log("hi")
+  // Initialize canvas
   canvas = document.getElementById("mainCanvas");
   ctx = canvas.getContext("2d");
 
+  // Make sure the canvas fits in the screen
   length = Math.min(window.innerWidth, window.innerHeight) * 0.8;
   canvas.width = length;
   canvas.height = length;
 
+  // Load the number tiles by putting them into the html
   for (let i = 2; i <= 2048; i <<= 1) {
+    // We first add the number into our ImagePaths
+    imagePaths["" + i] = numberImagePrefix + i
+
+    // Then create the actual element
     var image = document.createElement("img")
     image.src = "./images/tiles/" + i + ".png"
-    imagePaths["" + i] = numberImagePrefix + i
     image.id = numberImagePrefix + i
     document.getElementById("images").appendChild(image)
   }
 
+  // Load the images into js
   for (var key in imagePaths) {
     images[key] = document.getElementById(imagePaths[key]);
   }
@@ -55,7 +72,10 @@ function initialize() {
 
 // Rendering the array
 function render_grid(numbers){
+  // Draw the background
   drawBackground();
+
+  // Render the cell of each element in our numbers
   for(let y = 0; y < numbers.length; ++y){
     for(let x = 0; x < numbers.length; ++x){
       if(numbers[y][x] == 0){
@@ -69,20 +89,28 @@ function render_grid(numbers){
 // movingPieces = [[number, start Cell Row, start Cell Column, end Cell Row, end Coll Column]]
 // createdPieces = [[number, cell row, cell column]]
 function encode(movingPieces, createdPieces){
+  // The encoded version of movingPieces
+  // [[number, start percentage x, start percentage y, end perc x, end perc y]]
   movingParts = []
   for(let piece of movingPieces){
     let [sx, sy] = getPCoordFromCell(piece[2], piece[1], 1)
     let [ex, ey] = getPCoordFromCell(piece[4], piece[3], 1)
     movingParts.push([piece[0], sx, sy, ex, ey])
   }
+
+  // The encoded version of createdPieces
+  // [[number, grid x, grid y]]
   createdParts = []
   for(let piece of createdPieces){
     createdParts.push([piece[0], piece[2], piece[1]]);
   }
+
+  // Then we return the two
   return [movingParts, createdParts]
 }
 
 function render_animation(movingParts, creationParts){
+  // We start by rendering the movement
   handlingAnimation = true;
   render_moving(movingParts, 0, creationParts)
 }
@@ -91,6 +119,7 @@ function render_animation(movingParts, creationParts){
 
 // parts = [[number, startx, starty, endx, endy]]
 function render_moving(parts, currentFrame, creation_parts){
+  // Once the animation completes, we go on to animate the creation of tiles
   if(currentFrame > animationMovingFrames){
     completed = []
     for(let part of parts){
@@ -99,31 +128,46 @@ function render_moving(parts, currentFrame, creation_parts){
     setTimeout(render_creation, animationDelta, creation_parts, completed, 0)
     return
   }
+
+  // If we need to draw, we start by drawing the background
   drawBackground()
+
+  // For each part, we draw its position based on its final coords, start coords and the current frame in the animation
   for(let part of parts){
     ratio = currentFrame / animationMovingFrames
     let [number, startx, starty, endx, endy] = part
     drawImageP(ratio * (endx - startx) + startx, ratio * (endy - starty) + starty, numberWidth, numberWidth, 1 << number)
   }
+
+  // After some time, we go on to the next frame
   setTimeout(render_moving, animationDelta, parts, ++currentFrame, creation_parts)
 }
 
-// parts = [[number, x, y]]; fullParts = [[number, x, y]]
+// parts = [[number, cell x, cell y]]; fullParts = [[number, perc x, perc y]]
 function render_creation(parts, fullParts, currentFrame){
+  // Once we're done with the creation animation
   if(currentFrame > animationCreationFrames){
     handlingAnimation = false;
     return
   }
+
+  // Start by drawing background
   drawBackground()
+
+  // When drawing created tiles, we still need to keep the tiles already there
   for(let part of fullParts){
     [number, x, y] = part
     drawImageP(x, y, numberWidth, numberWidth, 1 << number)
   }
+
+  // For every created tile, we draw the tile where it's supposed to be, but smaller in terms of the current frame
   for(let part of parts){
     let ratio = currentFrame / animationCreationFrames;
     let [number, x, y] = part;
     drawCellScaled(x, y, 1 << number, ratio);
   }
+
+  // Wait a bit, and go to the next frame
   setTimeout(render_creation, animationDelta, parts, fullParts, ++currentFrame)
 }
 
@@ -131,9 +175,12 @@ function render_creation(parts, fullParts, currentFrame){
 
 // Draw the lines of the thingymabober
 function drawLines() {
+  // Width is the total amount we have to work with
   var width = 100 - lineWidthP;
+  // Increase is the intervals at which we draw the lines
   var increase = width / numSquares;
 
+  // At every interval, we draw the corresponding x and y line
   for (var i = 1, x = increase; i < numSquares; i++, x += increase) {
     fillRectP(x, 0, lineWidthP, length, lineColor);
     fillRectP(0, x, length, lineWidthP, lineColor);
@@ -142,6 +189,7 @@ function drawLines() {
 
 // Draw an image at some place, image is just id
 function drawImage(x, y, width, height, image, opacity) {
+  // We draw the image, making sure that we also reset the opacity if we want semi-tranparency
   ctx.globalAlpha = opacity;
   ctx.drawImage(images[image], x, y, width, height);
   ctx.globalAlpha = 1
@@ -149,6 +197,7 @@ function drawImage(x, y, width, height, image, opacity) {
 
 // drawImage but for the resizables, image is just id
 function drawImageP(x, y, width, height, image, opacity = 1) {
+  // We convert the coordinates out of 100 into global coordinates based on canvas length
   ratio = length * 0.01
   drawImage(x * ratio, y * ratio, width * ratio, height * ratio, image, opacity)
 }
@@ -161,6 +210,7 @@ function fillRect(x, y, width, height, color) {
 
 // fillRect but inputs are in percentages; out of 100
 function fillRectP(x, y, width, height, color) {
+  // Convert coordinates out of 100 into a global coordinate based on length
   ratio = length * 0.01
   fillRect(x * ratio, y * ratio, width * ratio, height * ratio, color)
 }
@@ -170,12 +220,15 @@ function drawCell(x, y, value){
 }
 
 function drawCellScaled(x, y, value, scale){
+  // We get the Percentage coordinate given the current "cell coordinates"
   [x, y] = getPCoordFromCell(x, y, scale)
 
+  // Then simply draw the elements with the size scaled appropriately
   drawImageP(x, y, numberWidth * scale, numberWidth * scale, value)
 }
 
 function getPCoordFromCell(x, y, scale){
+  // spaghetti code for getting the coordinate out of 100 when given the cell coordinates
   x = x * cellInterval + lineWidthP / 2 + cellMarginFromLineCenter + (numberWidth - numberWidth * scale) / 2
   y = y * cellInterval + lineWidthP / 2 + cellMarginFromLineCenter + (numberWidth - numberWidth * scale) / 2
   return [x, y]
@@ -183,6 +236,7 @@ function getPCoordFromCell(x, y, scale){
 
 // Draws the background
 function drawBackground(){
+  // Draw the background of the game; an empty grid
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   drawImageP(0, 0, 100, 100, "border")
   drawLines();
@@ -191,10 +245,12 @@ function drawBackground(){
 function fillTextP(text, size, centerX, centerY, color, opacity = 1){
   ratio = length * 0.01
 
+  // Set the font that we will be using based on params
   ctx.fillStyle = color
   ctx.font = size * ratio + "px Arial bold"
   ctx.textAlign = "center";
 
+  // If we require transparency, make sure to undo it after we draw the text
   ctx.globalAlpha = opacity
   ctx.fillText(text, centerX * ratio, centerY * ratio)
   ctx.globalAlpha = 1
--- 2048/index.js
+++ 2048/index.js
@@ -3,41 +3,63 @@ var numbers = [[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
 const numSquares = 4;
 
+// Client when playing the game 2048 itself
 class gameClient{
   constructor(freeplay = false){
+    // In freeplay, we don't transition to the finishedClient
     this.freeplay = freeplay
   }
   update(){
+    // Update the score
     document.getElementById("score").innerHTML = "Score: " + this.getSum();
+
+    // Don't interrupt animations
     if(!handlingAnimation){
       render();
     }
+
+    // If every move results in other being changed, then the player loses
     if(!this.hasMoves()){
       currentClient = new finishedClient(false)
       return
     }
   }
+
+  // Returns the sum of the elements in numbers
   getSum(){
     let s = 0;
     for(let i = 0; i < numSquares; ++i){
       for(let c = 0; c < numSquares; ++c){
+        // 1 << numbers[i][c], because the numbers displayed on the screen are actually 2 ^ {element}, not the element itself
         s += numbers[i][c] != 0 ? 1 << numbers[i][c] : 0
       }
     }
     return s;
   }
+
+  // Returns if the player can make any more moves
   hasMoves(){
+    // Moves in each of the directions
     for(let direction of ["ArrowRight", "ArrowLeft", "ArrowDown", "ArrowUp"]){
       let tma = []
       let tca = []
+      // Moves with a copy of the numbers, stores the movements & creations in tma & tca
       move(direction, tma, tca, JSON.parse(JSON.stringify(numbers)))
-      if(!tma.every(e => e[1] == e[3] && e[2] == e[4])){
+
+      // If every element in tma isn't actually a move(for example (1,2) -> (1,2); nothing changes)
+      let didChange = tma.every(e => e[1] == e[3] && e[2] == e[4])
+
+      // If the current move did actually change something, then the user still has possible moves
+      if(!didChange){
         return true
         break
       }
     }
+    // None of the moves were meaningful, so the user has no more moves
     return false
   }
+
+  // Handles key events for the game
   keydown(e){
     // Don't move if currently animating
     if(handlingAnimation){
@@ -73,6 +95,8 @@ class gameClient{
     // Check to see if there's a 2048
     for(let i = 0; i < numSquares; ++i){
       for(let c = 0; c < numSquares; ++c){
+        // If there is, then if we're in freeplay mode, we just continue;
+        // Otherwise, we give them the "win" screen
         if(numbers[i][c] == 11 && !this.freeplay){
           currentClient = new finishedClient(true)
           return
@@ -86,15 +110,20 @@ class gameClient{
   }
 }
 
+// Client after the 2048 is finished; either player won or lost
 class finishedClient{
   constructor(didWin){
     this.win = didWin;
   }
 
   update(){
+    // We first render the board in the background
     render()
+
+    // Draw the border
     drawImageP(10, 10, 80, 80, "textBox", 0.6)
 
+    // We change the text based on if the player won
     let text = "You Lost!"
     let contText = "Press any key to restart"
     if(this.win){
@@ -102,11 +131,13 @@ class finishedClient{
       contText = "Press any key to continue playing"
     }
 
+    // Put the text on the screen
     fillTextP(text, 10, 50, 50, "black")
     fillTextP("Press any key to continue", 5, 50, 70, "black")
   }
 
   keydown(e){
+    // Press any key to continue; we only restart if the player lost
     if(!this.win){
       numbers = [[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
     }
@@ -114,8 +145,10 @@ class finishedClient{
   }
 }
 
+// Using extrapolation to deal with the current client
 var currentClient = new gameClient();
 
+// MAIN FUNCTION
 window.onload = function() {
   initialize();
 
@@ -124,16 +157,21 @@ window.onload = function() {
   setTimeout(update, 50)
 }
 
+// consistent updating - tick + render
 function update(){
+  // Is called every 50 milliseconds
   currentClient.update()
   setTimeout(update, 50)
 }
 
+// rendering
 function render(){
   render_grid(numbers)
 }
 
+// Given the "arrow" of the input, returns [row, column], the direction in the "step" of the arrow
 function getIntervalFromDirection(direction){
+  // is in form (rowInterval, columnInterval)
   switch(direction){
     case "ArrowRight":
       return [0, 1]
@@ -152,35 +190,47 @@ function getIntervalFromDirection(direction){
   }
 }
 
+// We either move from the bottom right and progress to the top left or vice versa; given the input direction, this function returns which of the two it is
 function getStartingConsider(direction){
   if(direction == "ArrowRight" || direction == "ArrowDown"){
+    // If we move right or down, then processing occurs starting at bot right
     return [numSquares - 1, numSquares - 1, -1, -1]
   }else{
+    // Otherwise, processing starts at top left
     return [0, 0, 1, 1]
   }
 }
 
+// Given the "arrow" of the input, we move the entire board in the direction
+// movingArray: the array of starting & ending points for each element - function alters this for render_animation
+// creationArray: the array for which elements are newly created - function alters this for render_animation
 function move(direction, movingArray, creationArray, numbers){
   var [rowInterval, colInterval] = getIntervalFromDirection(direction)
 
   var [startingRow, startingCol, rowMove, colMove] = getStartingConsider(direction)
 
+  // We move from the bot-right to top-left or vice versa, depending on direction
   for(var sr = startingRow; numSquares > sr && sr >= 0; sr += rowMove){
     for(var sc = startingCol; numSquares > sc && sc >= 0; sc += rowMove){
+      // If there's an element, we keep track of the "current" coordinates
       if(numbers[sr][sc] == 0){
         continue
       }
       var c = sc
       var r = sr
       while(true){
+        // The coordinates in the next step
         var nc = c + colInterval
         var nr = r + rowInterval
+        // If out of bounds, then element goes to (r, c)
         if(!(0 <= nc && nc < numSquares && 0 <= nr && nr < numSquares)){
           [numbers[r][c], numbers[sr][sc]] = [numbers[sr][sc], numbers[r][c]]
           movingArray.push([numbers[r][c], sr, sc, r, c])
           break
         }
+        // If the new coordinates "hit" another element
         if(numbers[nr][nc] != 0){
+          // If type is equal, combine
           if(numbers[nr][nc] == numbers[sr][sc]){
             movingArray.push([numbers[sr][sc], sr, sc, nr, nc])
             numbers[nr][nc]++
@@ -188,23 +238,27 @@ function move(direction, movingArray, creationArray, numbers){
             creationArray.push([numbers[nr][nc], nr, nc])
             break;
           }else{
+            // If type is unequal, we stop right before it
             [numbers[r][c], numbers[sr][sc]] = [numbers[sr][sc], numbers[r][c]]
             movingArray.push([numbers[r][c], sr, sc, r, c])
             break
           }
         }
+        // Update the current coordinates
         [c, r] = [nc, nr]
       }
     }
   }
-  return true
 }
 
+// Handles when a key is pressed down
 function keyDown(e){
   currentClient.keydown(e);
 }
 
+// returns a random empty coordinate
 function randomEmpty(){
+  // All of the possible coordinates
   possibles = []
   for(let r = 0; r < numSquares; ++r){
     for(let c = 0; c < numSquares; ++c){
@@ -213,5 +267,7 @@ function randomEmpty(){
       }
     }
   }
+
+  // return a random one
   return possibles[Math.floor(Math.random() * possibles.length)]
 }
