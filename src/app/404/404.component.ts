8b0be023cf644b1397d8ecb466a76d9233250881
Daniel Ye <danielyeqt@gmail.com>
2024-10-14 12:43:01 -0400

    glitching

--- src/app/404/404.component.ts
+++ src/app/404/404.component.ts
@@ -1,5 +1,5 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Component, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-404',
@@ -9,8 +9,38 @@ import { Component, PLATFORM_ID } from '@angular/core';
   styleUrl: './404.component.scss'
 })
 export class Component404 {
+  glitchPosition: string = "0 0";
+  intervalId: any;
+  platformId: Object;
+
+  constructor(@Inject(PLATFORM_ID) platformId: Object) {
+    this.platformId = platformId;
+  }
 
   ngOnInit() {
+    if(isPlatformBrowser(this.platformId)) {
+      this.runAnimation();
+    }
+  }
+
+  runAnimation() {
+    let curframe = 0;
+    const totalFrames = 40;
+    let cx = 0, cy = 0;
+    this.intervalId = setInterval(() => {
+      if(curframe == 0) cx = Math.random() * 400 - 200, cy = -Math.random() * 500;
+      if(curframe >= 36) {
+        cx = Math.random() * 400 - 200; 
+        cy = -Math.random() * 500;
+      }
+      this.glitchPosition = `${cx}px ${cy}px`;
+      curframe = (curframe + 1) % totalFrames;
+    }, 50);
   }
   
+  ngOnDestroy() {
+    if(this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
