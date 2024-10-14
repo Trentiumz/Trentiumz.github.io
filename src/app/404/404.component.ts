1b7a3eefc61166143aede409b12ba24230cc362a
Daniel Ye <danielyeqt@gmail.com>
2024-10-14 13:20:05 -0400

    Sad Blob in 404

--- src/app/404/404.component.ts
+++ src/app/404/404.component.ts
@@ -10,6 +10,8 @@ import { Component, Inject, PLATFORM_ID } from '@angular/core';
 })
 export class Component404 {
   glitchPosition: string = "0 0";
+  blobleft: string = "0";
+  blobtop: string = "0";
   intervalId: any;
   platformId: Object;
 
@@ -27,13 +29,21 @@ export class Component404 {
     let curframe = 0;
     const totalFrames = 40;
     let cx = 0, cy = 0;
+    let blobX=0, blobY=0;
     this.intervalId = setInterval(() => {
-      if(curframe == 0) cx = Math.random() * 400 - 200, cy = -Math.random() * 500;
+      if(curframe == 0) {
+        cx = Math.random() * 400 - 200, cy = -Math.random() * 500;
+        blobX = blobY = 0;
+      }
       if(curframe >= 36) {
         cx = Math.random() * 400 - 200; 
         cy = -Math.random() * 500;
+        blobX = Math.random() * 400 - 200;
+        blobY = Math.random() * 400 - 200;
       }
       this.glitchPosition = `${cx}px ${cy}px`;
+      this.blobleft = `${blobX}px`;
+      this.blobtop = `${blobY}px`;
       curframe = (curframe + 1) % totalFrames;
     }, 50);
   }
