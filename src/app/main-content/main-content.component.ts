a9de0a7f8388fb0d2a72f9b42b27c3a4a86ac277
Daniel Ye <danielyeqt@gmail.com>
2025-02-16 17:01:24 -0500

    Add SSR icon angles (no ResizeObserver)

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -10,6 +10,8 @@ import { AchievementCardComponent } from '../achievement-card/achievement-card.c
 })
 export class MainContentComponent {
   @ViewChild("imageContainer") imageContainer!: ElementRef;
+
+  // angles at which to position image icons!
   angle: { [key: string]: number } = {
     "icon-linkedin": 60,
     "icon-github": 0,
@@ -18,23 +20,33 @@ export class MainContentComponent {
   }
 
   ngAfterViewInit() {
+    // icon repositioning!
     let imageContainerElement: Element = this.imageContainer.nativeElement;
     let icons: HTMLElement[] = Array.from(imageContainerElement.querySelectorAll('.social-image'));
+
+    // function for putting icons in a circle
     const repositionIcons = () => {
       for(let icon of icons) {
         let xPos = imageContainerElement.clientWidth / 2 * (1 + Math.cos(this.angle[icon.id] * Math.PI / 180)) - icon.clientWidth / 2;
         let yPos = imageContainerElement.clientHeight / 2 * (1 + Math.sin(this.angle[icon.id] * Math.PI / 180)) - icon.clientHeight / 2;
         icon.style.left = `${xPos}px`;
         icon.style.top = `${yPos}px`;
-        console.log(`${icon.id}: ${xPos} ${yPos}`);
       }
     }
-    const iconPositioner = new ResizeObserver(entries => {
+
+    // server side rendering fallback; add event listener for when user resizes an object
+    if (typeof ResizeObserver === 'undefined') {
       repositionIcons();
-    })
-    iconPositioner.observe(imageContainerElement)
-    for(let icon of icons) {
-      iconPositioner.observe(icon);
+      imageContainerElement.addEventListener('resize', repositionIcons);
+      for(let icon of icons) {
+        icon.addEventListener('resize', repositionIcons);
+      }
+    } else {
+      // ideal implementation - repositions upon any resizing
+      const iconPositioner = new ResizeObserver(() => {
+        repositionIcons();
+      })
+      iconPositioner.observe(imageContainerElement)
     }
   }
 }
