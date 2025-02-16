cdf2dc2d6c2abb13c6c9a72da4b8998b7ed7a1a9
Daniel Ye <danielyeqt@gmail.com>
2025-02-16 16:47:22 -0500

    Add icons!

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 import { AchievementCardComponent } from '../achievement-card/achievement-card.component';
 
 @Component({
@@ -9,5 +9,33 @@ import { AchievementCardComponent } from '../achievement-card/achievement-card.c
   styleUrl: './main-content.component.scss'
 })
 export class MainContentComponent {
+  @ViewChild("imageContainer") imageContainer!: ElementRef;
+  angle: { [key: string]: number } = {
+    "icon-linkedin": 60,
+    "icon-github": 0,
+    "icon-codeforces": -60,
+    "icon-dmoj": -120
+  }
 
+  ngAfterViewInit() {
+    let imageContainerElement: Element = this.imageContainer.nativeElement;
+    let icons: HTMLElement[] = Array.from(imageContainerElement.querySelectorAll('.social-image'));
+    const repositionIcons = () => {
+      for(let icon of icons) {
+        let xPos = imageContainerElement.clientWidth / 2 * (1 + Math.cos(this.angle[icon.id] * Math.PI / 180)) - icon.clientWidth / 2;
+        let yPos = imageContainerElement.clientHeight / 2 * (1 + Math.sin(this.angle[icon.id] * Math.PI / 180)) - icon.clientHeight / 2;
+        icon.style.left = `${xPos}px`;
+        icon.style.top = `${yPos}px`;
+        console.log(`${icon.id}: ${xPos} ${yPos}`);
+      }
+    }
+    const iconPositioner = new ResizeObserver(entries => {
+      repositionIcons();
+    })
+    iconPositioner.observe(imageContainerElement)
+    for(let icon of icons) {
+      iconPositioner.observe(icon);
+    }
+  }
 }
+
